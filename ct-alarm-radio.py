bdca442607676e2d18f2844b068fc6ae8f536c9c
christian@voegtle.org <christian@voegtle.org>
2020-02-02 22:13:05 +0100

    Revert "Während der Zeiteinstellung nicht in den Idlescreen wechseln. Nachtrag"
    This reverts commit 5721c4b7

--- ct-alarm-radio.py
+++ ct-alarm-radio.py
@@ -319,7 +319,7 @@ class application:
             self.current_screen = self.alarmscreen
             return
 
-        if self.is_idle and self.current_screen == self.clockscreen:
+        if self.is_idle and self.current_screen == self.clockscreen():
             self.ui.redraw = True
             self.current_screen = self.idlescreen
             return
